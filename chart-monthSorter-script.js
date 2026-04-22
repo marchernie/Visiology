ea34a145e57743dd044e1f7045bb5095234ce3ff
Mary Chernova <115955630+marchernie@users.noreply.github.com>
2026-04-22 20:14:03 +0300

    Add chart-monthSorter-script.js

---
+++ chart-monthSorter-script.js
@@ -0,0 +1,29 @@
+// Словарь сортировки для текстовых месяцев
+const monthOrder = {
+  'Янв': 1, 'Фев': 2, 'Март': 3, 'Апр': 4, 'Май': 5, 'Июн': 6,
+  'Июл': 7, 'Авг': 8, 'Сен': 9, 'Окт': 10, 'Ноя': 11, 'Дек': 12
+};
+
+// Собираем данные вместе
+let pairedData = w.xAxis.data.map((month, i) => ({
+  month: month,
+  value: w.series[0].data[i]
+}));
+
+// Сортируем по месяцам
+pairedData.sort((a, b) => monthOrder[a.month] - monthOrder[b.month]);
+
+// Возвращаем обратно в массивы
+w.xAxis.data = pairedData.map(d => d.month);
+w.series[0].data = pairedData.map(d => d.value);
+
+ChartRender({
+  general: w.general,
+  xAxis: w.xAxis,
+  yAxis: w.yAxis,
+  series: w.series,
+  legend: w.legend,
+  tooltip: w.tooltip,
+  grid: w.grid,
+  dataZoom: w.dataZoom
+});
